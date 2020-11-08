f64e8d756f7f8ed7f88d3970dc5dc652afe1fb54
Brian Glowniak <bglowniak@gmail.com>
2020-11-08 12:52:16 -0500

    Add logging and reorganize a bit

--- ronald.py
+++ ronald.py
@@ -1,15 +1,19 @@
 import discord
 from discord.ext.commands import Bot, command, Cog
+import logging
 
 class Ron(Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%H:%M:%S')
+
         self.add_command(self.placeholder)
 
-    # EVENTS
+    #################################    EVENTS    #################################
+
     async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
+        logging.info(f'{self.user} has connected to Discord!')
 
     # override on_message to implement some functionality outside of normal commands
     async def on_message(self, message):
@@ -19,15 +23,22 @@ class Ron(Bot):
         content = message.content.strip().lower()
         author_mention = "<@!" + str(message.author.id) + ">"
 
-        if content == "hello":
-            await message.channel.send("salve")
+        # no functionality here yet
 
         # once we have checked the full message, process any commands that may be present
         await self.process_commands(message)
 
-    # COMMANDS
+    # add help command functionality at some point
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, CommandNotFound):
+            logging.warning(f"Command in message \"{ctx.message.content}\" not found. Ignoring.")
+            return
+        raise error
+
+    # COMMANDS#################################    COMMANDS    #################################
+
     @command(name="placeholder")
     async def placeholder(ctx, arg1, *args):
         pass
 
-    # HELPERS
+    #################################    HELPERS    #################################
